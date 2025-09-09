769359e7bbf734e08313bbe78a9ff3912394ffaf
Jaad <jaadouaich@hotmail.be>
2025-09-09 10:11:06 +0200

    feat: magic button that changes a paragraph to an other

--- Ex 2/dom-manipulation.js
+++ Ex 2/dom-manipulation.js
@@ -1,3 +1,15 @@
 export const changeParagraph = () => {
+  const paragraph = document.getElementById('magic-p');
+  console.log(paragraph);
+  if(paragraph.textContent === "Hello everybody, I'm just a little paragraph..." || paragraph.textContent === "Wait, I'm not goated anymore..."){
+    paragraph.textContent = "I AM THE MAGIC GOATED PARAGRAAAAAPH";    
+  } else {
+    paragraph.textContent = "Wait, I'm not goated anymore...";
+  }  
+  
+};
 
-}
+document.addEventListener('DOMContentLoaded', () =>{
+  const button = document.querySelector("button");
+  button.addEventListener('click', changeParagraph);
+})
